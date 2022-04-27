5960c1112da03d40c3f37063a4b2c0f44e8f26c8
Mamay-ka <mamay_ka@mail.ru>
2022-04-27 20:01:49 +0300

    ДЗ работающая турель

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float BulletSpeed = 1;
+            
+    public Vector3 TargetPos 
+    {
+        get => _targetPos;
+        set
+        {
+            _isShooted = true;
+            _targetPos = value;
+        }
+    }
+
+    private bool _isShooted = false;
+    private Vector3 _targetPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_isShooted && Vector3.Distance(transform.position, _targetPos) > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetPos, BulletSpeed * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -10,9 +10,15 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField] float VisionRadius = 10;
     [SerializeField] Transform Ghost;
     [SerializeField] Transform Sphere;
+    [SerializeField] Transform AmmoHead;
+    [SerializeField] float RotationSpeed = 1;
+    [SerializeField] GameObject BulletPrefab;
+    [SerializeField] float CooldownTime;
    
 
     private SphereCollider _visionCollider;
+    private Transform _playerPos;
+    private float _currentTime;
 
     // Start is called before the first frame update
 
@@ -24,16 +30,49 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         _visionCollider.radius = VisionRadius;
+        _currentTime = CooldownTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_playerPos != null)
+        {
+            _currentTime += Time.deltaTime;
+            var rotDirection = Ghost.position - Sphere.position;
+            Sphere.rotation = Quaternion.LookRotation(rotDirection.normalized);
+        }
 
-        var rotDirection = Ghost.position - Sphere.position;
-
-        //Turret.forward = rotDirection; 
-        Sphere.rotation = Quaternion.LookRotation(rotDirection.normalized);
         
+        
+
+
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if(other.CompareTag("Player")) 
+        {
+            _playerPos = other.gameObject.transform;
+        }
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && _currentTime >= CooldownTime)
+        {
+            var bullet = Instantiate(BulletPrefab, AmmoHead.position, Quaternion.identity);
+            bullet.GetComponent<Bullet>().TargetPos = _playerPos.position;
+            _currentTime = 0;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            _playerPos = null;
+        }
     }
 }
