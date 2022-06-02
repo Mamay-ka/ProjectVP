170be52d158b1be7a977d737f3d5ecb177902e6f
Mamay-ka <mamay_ka@mail.ru>
2022-06-02 19:49:01 +0300

    ДЗ-2
    1. Добавлен HealthBar посредством GUI. Здоровье отображается цифрами и полоской. У входа в пещеру есть мины, рядом точка здоровья, чтобы проверить работоспособность.
    2. Добавлен четвертый слайдер rgbA и minValue для слайдера MySlider. Если я правильно понял задание, слайдер становиться активным только со значения, переданного в переменную minValue.

--- Assets/Scripts/GUI/GUIHealths.cs
+++ Assets/Scripts/GUI/GUIHealths.cs
@@ -4,17 +4,19 @@ using UnityEditor;
 
 public class GUIHealths : MonoBehaviour
 {
-    //[SerializeField] HealthController HealthController;
-    //float _GuiHealth = GetComponent<HealthController>()._currentHealth;
-    private string Text = "150";
-
+    public HealthController _healthController = null;//ссылка на скрипт, который висит на объекте и с которым я собираюсь работать.
+    public GameObject Cil = null;//ссылка на объект, на котором висит скрипт.
+    private string Text = "150";// переменная, которая будет выводит уровень здоровья.
+    private float _healthBar = 0.0f;
 
 
 
     void OnGUI()
     {
-        
-        GUI.Box(new Rect(Screen.width/2 - 100, 0, 200, 25), Text);
+        _healthController = Cil.GetComponent<HealthController>();//получение доступа к скрипту HealsController на объекте Cil.
+        Text = _healthController.CurrentHealth.ToString();//получение доступа к переменной CurrentHealth и приведение ее к строковому типу. 
+        _healthBar = _healthController.CurrentHealth;
+        GUI.Box(new Rect(Screen.width/2 - 100, 0, (_healthBar * 2), 25), Text);//создание окна в нужном месте заданного размера и вывод информации внутри. 
        
     }
 }
